8a0e69881cebb337e0b6f2300345d838577e21e7
lucasbarbosafs <lucas.barbosa.fs@gmail.com>
2020-12-09 15:48:33 -0300

    Criacao da factory para o model 'Profile', que sera utilizada na execucao dos testes

---
+++ spec/factories/profiles.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :profile do
+    blood_type { Faker::Blood.type }
+    weight { Faker::Number.normal(mean: 90, standard_deviation: 10.5) }
+    height { Faker::Number.between(from: 150, to: 190) }
+    rh_factor { Faker::Blood.rh_factor }
+    name { Fakes::Name.name }
+    birthday { Faker::Date.birthday(min_age: 18) }
+    cell_phone { Faker::PhoneNumber.cell_phone_in_e164 }
+    user
+  end
+end
