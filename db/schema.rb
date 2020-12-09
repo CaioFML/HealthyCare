87c48db5077601ca7f47601b9730bdcaad4a723d
lucasbarbosafs <lucas.barbosa.fs@gmail.com>
2020-12-08 22:19:43 -0300

    Criacao da factory para o model user que sera utilizada nos testes. Criacao dos testes de model para User. Adicao do atributo name na migrate do model User. Adicao das validacoes no User

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2020_12_09_003436) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
