0bede1e6cd40b1b506538950f6058dda9802f69d
lucasbarbosafs <lucas.barbosa.fs@gmail.com>
2020-12-10 11:23:44 -0300

    Refatoracao e adicao de contextos separados para os testes de validacao do atributo 'birthday' no profile_spec

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,30 +1,41 @@
 RSpec.describe Profile do
   describe "validations" do
-    subject(:profile) { build(:profile) }
-
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:cell_phone) }
     it { is_expected.to validate_presence_of(:birthday) }
     it { is_expected.to validate_numericality_of(:weight).is_greater_than(0) }
     it { is_expected.to validate_numericality_of(:height).is_greater_than(0) }
     it { is_expected.to belong_to :user }
+  end
 
-    context "when birthday" do
-      it "is valid with past birthday" do
-        profile.birthday = Time.zone.today - 1
-        profile.valid?
+  describe "birthday" do
+    subject(:profile) { build(:profile) }
+
+    before do
+      profile.birthday = date
+      profile.valid?
+    end
+
+    context "when is in the past" do
+      let(:date) { 1.day.ago }
+
+      it "does not add an error" do
         expect(profile.errors.keys).not_to include :birthday
       end
+    end
 
-      it "is valid with current birthday" do
-        profile.birthday = Time.zone.today
-        profile.valid?
+    context "when is today" do
+      let(:date) { Time.zone.today }
+
+      it "does not add an error" do
         expect(profile.errors.keys).not_to include :birthday
       end
+    end
 
-      it "is invalid with future birthday" do
-        profile.birthday = Time.zone.today + 1
-        profile.valid?
+    context "when is in the future" do
+      let(:date) { 1.day.from_now }
+
+      it "adds an error" do
         expect(profile.errors.keys).to include :birthday
       end
     end
