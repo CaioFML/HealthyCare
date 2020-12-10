d24194aa62bf01ded37f07a1b6ecfb2d01a53847
Marcelo Pinheiro <marcelo.lannorte@gmail.com>
2020-12-10 08:59:52 -0300

    criação do factor_bot appointments

---
+++ spec/factories/appointments.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :appointment do
+    professional { Faker::Name.name }
+    date_time { 5.days.from_now }
+    specialty { "Clinico Geral" }
+    complaints { Faker::Lorem.paragraph }
+    diagnostics { Faker::Lorem.paragraph }
+  end
+end
