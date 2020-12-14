a1d626e97eb63fce43ae2cef74ccd3ea8fb53a23
Marcelo Pinheiro <marcelo.lannorte@gmail.com>
2020-12-14 10:41:51 -0300

    tela de login e dash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception, prepend: true
+
+    layout :seleciona_layout
+
+    protected
+      def seleciona_layout
+            if user_signed_in?
+                "admin"
+            else
+                "application"
+            end
+      end
 end
